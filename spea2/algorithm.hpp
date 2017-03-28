92aa5f5c06e48ff12c45a80a6ccbbe9cb3e14ead
Filipe Verri <filipeverri@gmail.com>
2017-03-28 14:16:55 -0300

    Cached fx inside solution.

--- spea2/algorithm.hpp
+++ spea2/algorithm.hpp
@@ -45,12 +45,13 @@ template <std::size_t N, typename T> class algorithm<N, T, meta::requires<Indivi
 public:
   struct solution_type
   {
-    individual_type individual;
+    individual_type x;
+    f_type fx;
     double fitness;
 
     friend auto operator<<(std::ostream& os, const solution_type& s) -> std::ostream&
     {
-      return os << s.individual << ",\tfitness = " << s.fitness;
+      return os << s.x << ",\tfitness = " << s.fitness;
     }
   };
 
@@ -65,11 +66,11 @@ public:
     next_population_.reserve(population.size());
     std::transform(population.begin(), population.end(),
                    std::back_inserter(next_population_), [](auto& ind) -> solution_type {
-                     return {std::move(ind), 0.0};
+                     auto fx = ind.f();
+                     return {std::move(ind), std::move(fx), 0.0};
                    });
 
     archive_.reserve(next_population_.size() + archive_size_);
-    fx_.reserve(next_population_.size() + archive_size_);
     strength_.reserve(population.size() + archive_size_);
     environmental_selection();
   }
@@ -90,10 +91,6 @@ public:
 private:
   auto environmental_selection() -> void
   {
-    std::transform(next_population_.cbegin(), next_population_.cend(),
-                   std::back_inserter(fx_),
-                   [](const auto& sol) { return sol.individual.f(); });
-
     std::move(next_population_.begin(), next_population_.end(),
               std::back_inserter(archive_));
 
@@ -108,12 +105,11 @@ private:
       auto is_nondominated = true;
       for (const auto j : util::range(0u, first_dominated))
       {
-        if (dominates(fx_[j], fx_[i]))
+        if (dominates(archive_[j].fx, archive_[i].fx))
         {
           // Individual i is dominated
           is_nondominated = false;
           --first_dominated;
-          std::swap(fx_[i], fx_[first_dominated]);
           std::swap(archive_[i], archive_[first_dominated]);
           break;
         }
@@ -129,19 +125,17 @@ private:
 
     if (past_nondominated < archive_size_)
     {
-      // TODO: fill out with the best dominated individuals
-
       // Strength:
       strength_.clear();
       std::fill_n(std::back_inserter(strength_), archive_.size(), 0.0);
 
-      const auto all_ix = util::indexes_of(archive_, strength_, fx_);
+      const auto all_ix = util::indexes_of(archive_, strength_);
       const auto dominated_ix = util::range(past_nondominated, archive_.size());
       const auto nondominated_ix = util::range(0u, past_nondominated);
 
       for (const auto i : all_ix)
         for (const auto j : dominated_ix)
-          if (dominates(fx_[i], fx_[j]))
+          if (dominates(archive_[i].fx, archive_[j].fx))
             ++strength_[i];
 
       // Raw Fitness:
@@ -150,18 +144,18 @@ private:
 
       for (const auto i : dominated_ix)
         for (const auto j : all_ix)
-          if (dominates(fx_[j], fx_[i]))
+          if (dominates(archive_[j].fx, archive_[i].fx))
             archive_[i].fitness += strength_[j];
 
       //  Distances:
       auto tree = util::rtree<N>{};
       for (const auto i : all_ix)
-        tree.insert(std::make_pair(util::to_point(fx_[i]), i));
+        tree.insert(std::make_pair(util::to_point(archive_[i].fx), i));
 
       // Fitness:
       for (const auto i : dominated_ix)
       {
-        const auto current = util::to_point(fx_[i]);
+        const auto current = util::to_point(archive_[i].fx);
         auto kth_nei = util::indexed_point<N>{};
 
         tree.query(util::nearest(current, k_), util::setter(kth_nei));
@@ -169,10 +163,10 @@ private:
       }
 
       // Partially sort dominated
-      std::partial_sort( //
+      std::partial_sort(                      //
         archive_.begin() + past_nondominated, //
-        archive_.begin() + archive_size_, //
-        archive_.end(), //
+        archive_.begin() + archive_size_,     //
+        archive_.end(),                       //
         [](const auto& x, const auto& y) { return x.fitness < y.fitness; });
     }
     else
@@ -188,9 +182,7 @@ private:
   const std::size_t population_size_, archive_size_, k_;
   const double mutation_rate_, crossover_rate_;
 
-  std::vector<f_type> fx_;
   std::vector<std::size_t> strength_;
-
   typename decltype(archive_)::const_iterator end_nondominated_;
 };
 
